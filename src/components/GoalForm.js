167049a0fe5f745331ff1f05bd5f1f88746a15ef
Maheeb <maheeb009@gmail.com>
2022-10-19 22:46:08 +0600

    simple validation added

--- src/components/GoalForm.js
+++ src/components/GoalForm.js
@@ -4,25 +4,66 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import {Col, Container, Row} from "react-bootstrap";
 import {useState} from "react";
+import '../css/styles.css';
+
+
+
+
+const styles = {
+    invalid: {
+        border: "1px solid red"
+
+    }
+
+};
+
+
 
 const GoalForm = (props) => {
     const [goal,updateGoal] = useState("");
-
+    const[color, setColor] = useState('');
     const getCourseGoals=(e)=>{
+
        updateGoal(e.target.value);
     }
 
     const GoalSubmit = (event) =>
     {
-        event.preventDefault();
-       const goals={
-           goal: goal
-       }
-        props.parentCallback(goals);
+
+            event.preventDefault();
+            const goals = {
+                goal: goal
+            }
+
+            if (goals.goal !== '') {
+                // alert('dsad')
+                props.parentCallback(goals);
+            }
+            else
+            {
+                // emptyItem();
+
+                setColor('invalid');
+
+                // alert('Please input your project goal !!')
+            }
+        // props.parentCallback(goals);
+
+
+        // let emptyItem =() =>{
+        //   return   this.setState({
+        //         inputClass: styles.invalid
+        //     })
+        // }
+
+
 
     }
+
     return (
         <div>
+
+
             <Container>
                 <Row>
                     <Col lg={12}>
@@ -34,7 +75,7 @@ const GoalForm = (props) => {
                                 <Card.Title>Project Goal</Card.Title>
                                 <Form action={""} onSubmit={GoalSubmit}>
                                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                                        <Form.Control type="text" placeholder="Enter Goal" name="name" onChange={getCourseGoals}/>
+                                        <Form.Control type="text" placeholder="Enter Goal" name="name" onChange={getCourseGoals} className={color}/>
 
                                     </Form.Group>
 
